ca9fdd06d5517a1a5bb94cb177ad33c8ad537efe
Erickson Fana Morales <erickson_fana@ucne.edu.do>
2021-02-08 18:47:36 -0400

    Subiendo el commit de la clase Contexto de la carpeta DAL

---
+++ DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_ap1_2018_0553.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_ap1_2018_0553.DAL
+{
+    class Contexto : DbContext
+    {
+
+        public DbSet<Ciudad> Ciudad { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = datosCiudades.Db");
+        }
+    }
+}
