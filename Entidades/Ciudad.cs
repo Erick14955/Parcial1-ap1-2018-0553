83b9e97e160784fb645b94785af1120afe65f732
Erickson Fana Morales <erickson_fana@ucne.edu.do>
2021-02-08 18:33:26 -0400

    Subiendo la clase ciudad de la carpeta entidades. Closes #2.

---
+++ Entidades/Ciudad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_ap1_2018_0553.Entidades
+{
+    class Ciudad
+    {
+        [Key]
+        public int CiudadID { get; set; }
+
+        public string Nombre_Ciudad { get; set; }
+    }
+}
