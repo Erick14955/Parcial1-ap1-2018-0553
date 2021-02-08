0d334bc1e98795617a0df0016e6a72e859e07536
Erickson Fana Morales <erickson_fana@ucne.edu.do>
2021-02-08 18:48:46 -0400

    Subiendo el commit de la Migracion

---
+++ Migrations/20210208224812_Inicial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parcial1_ap1_2018_0553.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ciudad",
+                columns: table => new
+                {
+                    CiudadID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre_Ciudad = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ciudad", x => x.CiudadID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ciudad");
+        }
+    }
+}
